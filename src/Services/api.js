0d7b0f15e061dc3ca69f073902c3837d29f28617
Vitor <vitormuller66@gmail.com>
2022-08-03 17:10:52 -0300

    feat: deploy configs

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000"
+const BASE_URL = "https://api-licitacoes.herokuapp.com"
 
 async function getBids() {
   const promise = await axios.get(`${BASE_URL}/bids`);
